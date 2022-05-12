faa36de1e52f3745ed5d51ba1df984bc29c4aeec
jcleston <janes.cleston.silva@gmail.com>
2022-05-12 11:00:19 -0300

    Atualização

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,9 @@
+import { Popover } from '@headlessui/react'
+
+export function CloseButton() {
+    return (
+        <Popover.Button className="top-5 right-5 absolute text-zinc-400">
+        
+        </Popover.Button>
+    );
+}
---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,16 @@
+export function WidgetForm() {
+    return (
+        <div className="gb-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+
+            </header>
+
+            <p>oi</p>
+
+            <footer>
+                Feito por Janes Cleston <a className="underline underline-offset-2" href="https://jcleston.github.io/github-page/">Github page</a>
+            </footer>
+        </div>
+    );
+}
