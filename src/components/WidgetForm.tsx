2ddbaee41e361c7bde003a75af00d832f0a15d2d
jcleston <janes.cleston.silva@gmail.com>
2022-05-12 12:09:20 -0300

    Atualização

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,14 +1,58 @@
+import { CloseButton } from "./CloseButton";
+
+import bugImageUrl from '../assets/bug.svg';
+import ideaImageUrl from '../assets/idea.svg';
+import thoughtImageUrl from '../assets/thought.svg';
+
+const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt: 'Imagem de um inseto'
+        },
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImageUrl,
+            alt: 'Imagem de uma lâmpada'
+        },
+    },
+    OTHER: {
+        title: 'Outro',
+        image: {
+            source: thoughtImageUrl,
+            alt: 'Imagem de um balão de pensamento'
+        },
+    },
+};
+
 export function WidgetForm() {
     return (
         <div className="gb-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
                 <span className="text-xl leading-6">Deixe seu feedback</span>
 
+                <CloseButton />
             </header>
 
-            <p>oi</p>
+            <div className="flex py-8 w-full">
+                { Object.entries(feedbackTypes).map(([key, value]) => {
+                    return (
+                        <button
+                            className=""
+                            onclick={}
+                            type="button"
+                        >
+                            <img src={value.image.source} alt={value.image.alt} />
+                            <span>{value.title}</span>
+                        </button>
+                    );
+                })}            
+            </div>
 
-            <footer>
+            <footer className="text-xs text-neutral-400">
                 Feito por Janes Cleston <a className="underline underline-offset-2" href="https://jcleston.github.io/github-page/">Github page</a>
             </footer>
         </div>
