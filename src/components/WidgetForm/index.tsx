642d76b86df7d75b28184d7b7f47dcb3b469608d
jcleston <janes.cleston.silva@gmail.com>
2022-05-16 14:33:50 -0300

    Atualização

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,10 +1,11 @@
-import { CloseButton } from "./CloseButton";
+import { useState } from "react";
+import { CloseButton } from "../CloseButton";
 
-import bugImageUrl from '../assets/bug.svg';
-import ideaImageUrl from '../assets/idea.svg';
-import thoughtImageUrl from '../assets/thought.svg';
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG: {
         title: 'Problema',
         image: {
@@ -28,29 +29,24 @@ const feedbackTypes = {
     },
 };
 
+export type FeedbackType = keyof typeof feedbackTypes;
+// 55:42
 export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+
             <header>
                 <span className="text-xl leading-6">Deixe seu feedback</span>
 
                 <CloseButton />
             </header>
 
-            <div className="flex py-8 gap-2 w-full">
-                { Object.entries(feedbackTypes).map(([key, value]) => {
-                    return (
-                        <button
-                            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            // onclick={}
-                            type="button"
-                        >
-                            <img src={value.image.source} alt={value.image.alt} />
-                            <span>{value.title}</span>
-                        </button>
-                    )
-                })}            
-            </div>
+            {!feedbackType ? (
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            ) : (
+                <p>oi</p>
+            )}
 
             <footer className="text-xs text-neutral-400">
                 Feito por Janes Cleston <a className="underline underline-offset-2" href="https://jcleston.github.io/github-page/">Github page</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // content: ["./src/**/*.tsx"],
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
