08b2535ece80c5dc2db2db405d74f97247c468a1
jcleston <janes.cleston.silva@gmail.com>
2022-05-17 12:18:36 -0300

    Atualização

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,17 +1,42 @@
+import { ArrowLeft } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
 
-export function FeedbackContentStep(){
+interface FeedbackTypeStepProps{
+    feedbackType: FeedbackType;
+    onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackContentStep({ 
+    feedbackType, 
+    onFeedbackRestartRequested 
+    }: FeedbackTypeStepProps){
+    const feedbackTypeInfo = feedbackTypes[feedbackType];
+
     return (
         <>
             <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
+                <button 
+                    type="button" 
+                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+                    onClick={onFeedbackRestartRequested}
+                    >
+                    <ArrowLeft weight="bold" className="w-4 h-4" />
+                </button>
+
+                <span className="text-xl leading-6 flex items-center gap-2">
+                    <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" /> 
+                    {feedbackTypeInfo.title}
+                </span>
 
                 <CloseButton />
             </header>
 
-            <div className="flex py-8 gap-2 w-full">
-                           
-            </div>
+            <form className="my-4 w-full">
+                <textarea
+                
+                />
+            </form>
         </>
     )
 }
