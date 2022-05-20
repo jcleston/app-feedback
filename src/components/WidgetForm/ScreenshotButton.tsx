f3e1de45ed6b3f8d32f6f64d8db4e6e16e062e53
jcleston <janes.cleston.silva@gmail.com>
2022-05-20 11:11:20 -0300

    Atualização

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -3,10 +3,10 @@ import html2canvas from 'html2canvas';
 import { useState } from "react";
 import { Loading } from "../Loading"
 
-export function ScreenshotButton(){
+export function ScreenshotButton() {
     const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
 
-    async function handleTakeScreenshot(){
+    async function handleTakeScreenshot() {
         setIsTakingScreenshot(true);
 
         const canvas = await html2canvas(document.querySelector('html')!);
@@ -18,13 +18,13 @@ export function ScreenshotButton(){
         setIsTakingScreenshot(false);
     }
 
-    return(
+    return (
         <button
             type="button"
             onClick={handleTakeScreenshot}
             className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
         >
-            { isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6" />}
+            {isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6" />}
         </button>
     )
 }
